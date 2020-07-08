f6790c512eee2ba32803bf58466ea81e31e98943
JezzaR <54776821+JezzaR-The-Proto@users.noreply.github.com>
2020-07-08 16:53:30 +0100

    Moved help to separate command

--- cogs/Commands.py
+++ cogs/Commands.py
@@ -15,16 +15,23 @@ class Commands(commands.Cog):
     async def on_ready(self):
         print('Providing e621 "Content"')
 
-    #Prints random e621 Content
+    #Help command
     @commands.command()
     @commands.cooldown(1,3,commands.BucketType.user)
-    async def yiff(self, ctx, type):
+    async def help(self, ctx):
         if not isinstance(ctx.channel, discord.DMChannel):
                 if not isinstance(ctx.channel, discord.GroupChannel):
                     if not ctx.channel.is_nsfw():
                         await ctx.send("Cannot be used in non-NSFW channels!")
                         return
-
+        await ctx.send("Here is a list of commands:")
+        await ctx.send("```**$yiff help**: Displays this message" + "\n" + "**$yiff random**: Shows random yiff from e621" + "\n" + "You can use any command after $yiff to specify tag" + "\n" + "eg: **$yiff gay** or **$yiff wolf**```")
+        await ctx.send("I hope you have a great time :wink:")
+    
+    #Random e621 content
+    @commands.command()
+    @commands.cooldown(1,3,commands.BucketType.user)
+    async def yiff(self, ctx, type):
         if type == "random":
             headers = {"User-Agent":"Project-Luna/1.0 (API Usage by jezzar on E621)"}
             Req = requests.get(f"https://e621.net/posts.json?tags=order:random&rating:explicit&limit=1", headers=headers)
@@ -32,29 +39,19 @@ class Commands(commands.Cog):
             if Req.status_code != 200:
                 await ctx.send(f"Couldn't contact e621. Error code: {Req.status_code}.\nJson: {ReqJson}")
                 return
-
             Post = ReqJson["posts"][0]["file"]["url"]
             await ctx.send(f"Here is your XD so Random yiff: {Post}")
             return
-        elif type == "help":
-            await ctx.send("Here is a list of commands:")
-            await ctx.send("```**$yiff help**: Displays this message" + "\n" + "**$yiff random**: Shows random yiff from e621" + "\n" + "You can use any command after $yiff to specify tag" + "\n" + "eg: **$yiff gay** or **$yiff wolf**```")
-            await ctx.send("I hope you have a great time :wink:")
         else:
             headers = {"User-Agent":"Project-Luna/1.0 (API Usage by jezzar on E621)"}
-            Req = requests.get(f"https://e621.net/posts.json?tags=order:random+{type}&rating:explicit&limit=1", headers=headers)
+            Req = requests.get(f"https://e621.net/posts.json?tags={type}+order:random&rating:explicit&limit=1", headers=headers)
             ReqJson = Req.json()
             if Req.status_code != 200:
                 await ctx.send(f"Couldn't contact e621. Error code: {Req.status_code}.\nJson: {ReqJson}")
                 return
-
             Post = ReqJson["posts"][0]["file"]["url"]
-            await ctx.send(f"Here is your {type} yiff: {Post}")
+            await ctx.send(f"Here is your XD so Random yiff: {Post}")
             return
-
-
-
-
 
 def setup(client):
     client.add_cog(Commands(client))
