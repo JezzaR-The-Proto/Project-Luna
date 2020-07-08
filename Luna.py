3681ba8e2c2a5e0c0bf25c7fdb267c86a001c715
JezzaR <54776821+JezzaR-The-Proto@users.noreply.github.com>
2020-07-08 16:54:10 +0100

    changed prefix
    and did the owner only load/unload thing

--- Luna.py
+++ Luna.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from humanfriendly import format_timespan
 
 #Sets the command prefix
-client = commands.Bot(command_prefix = '$yiff ')
+client = commands.Bot(command_prefix = '$')
 
 #Removes the default help command
 client.remove_command('help')
@@ -25,11 +25,13 @@ async def on_ready():
 
 #Command to load cogs
 @client.command()
+@commands.is_owner()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
 
 #Command to unload cogs
 @client.command()
+@commands.is_owner()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
